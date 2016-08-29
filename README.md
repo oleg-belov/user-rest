d9bcb0bf3e675c8460f726af35005b9fdceff743
sepi <sepi@ciklum.com>
2016-08-29 12:13:06 +0300

    app changed to support mongo db

---
+++ README.md
@@ -0,0 +1,42 @@
+== User rest
+
+Provide users notebook
+
+Available methods
+
+= Get all users
+
+GET /users
+
+= Get user by id
+
+GET /user/{id}
+
+= Add new user
+
+POST user/add
+
+```
+{
+  "id": "57c3f66dd4c63cf400447bda",
+  "username": "ivan",
+  "firstname": "ivanko",
+  "lastname": null,
+  "email": "ivan"
+}
+```
+
+= Update user
+
+PUT user/update/{id}
+
+```
+{
+  "id": "57c3f66dd4c63cf400447bda",
+  "username": "selin"
+}
+```
+
+= Delete user
+
+DELETE /user/delete/{id}
--- src/main/java/com/user/rest/controller/HelloController.java
+++ src/main/java/com/user/rest/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.user.rest.controller;
 
-import com.user.rest.entity.Message;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Message index() {
-        return new Message("Greetings from User Rest!");
+    @RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String index() {
+        return "Greetings from User Rest!";
     }
 }
